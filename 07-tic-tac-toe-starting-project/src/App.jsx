68772f3469f8814ef48aa70cbb141ca741838547
Hassan Raza <hassanraza@Hassans-MacBook-Pro.local>
2025-06-23 20:34:46 +0500

    fix: import capitalization

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,4 +1,4 @@
-import Player from "./components/player";
+import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
 import { WINNING_COMBINATIONS } from "./winning-combinations";
